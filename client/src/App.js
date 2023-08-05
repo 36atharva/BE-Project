8143d930a818f3d04aad0f2df4c430b123566628
Yugal Khanter <yugalkhanter82@gmail.com>
2023-08-05 14:36:56 +0530

    basic frontend folder structure set up

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-xl font-bold'>
+        Welcome to BE Project of Group 1!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
